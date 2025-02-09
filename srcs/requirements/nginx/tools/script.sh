98864de84c8750e32015425da00a1a80ad01d0c3
Zaid Kotbi <zkotbi@e2r12p4.1337.ma>
2025-02-09 17:50:59 +0100

    ok

---
+++ srcs/requirements/nginx/tools/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=MO/L=KH/O=1337/OU=student/CN=zkotbi.1337.ma"
+
+echo "server {
+	listen 443 ssl;
+	listen [::]:443 ssl;
+
+	server_name www.$DOMAIN_NAME $DOMAIN_NAME;
+
+	ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+	ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+
+	ssl_protocols TLSv1.3;
+
+	index index.php;
+	root /var/www/html;
+
+	location ~ [^/]\\.php(/|$) {
+        try_files $uri =404;
+        fastcgi_pass wordpress:9000;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    }
+}" > /etc/nginx/sites-available/default
+
+nginx -g "daemon off;"
