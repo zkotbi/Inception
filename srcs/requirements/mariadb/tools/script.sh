d9db5e4ed48e104c7f2962b4c1f072eaee0b07b9
Zaid Kotbi <zkotbi@e1r10p13.1337.ma>
2025-02-16 00:53:44 +0100

    almost done

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 
-# service mariadb start
+service mariadb start
 
-# sleep 2
+sleep 2
 
+mysql --user=root  -e "CREATE DATABASE IF NOT EXISTS $db_name;"
+mysql --user=root  -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd';"
+mysql --user=root  -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';"
+mysql --user=root  -e "FLUSH PRIVILEGES;"
+mysql --user=root  -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_pwd';"
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name;" > /tmp/init.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd';" >> /tmp/init.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';" >> /tmp/init.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_pwd';" >> /tmp/init.sql
-echo "FLUSH PRIVILEGES;" >> /tmp/init.sql
+kill $(cat /var/run/mysqld/mysqld.pid)
 
-# mysql --user=root -p"$db_root_pwd"  -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_pwd';"
-# mysql --user=root -p"$db_root_pwd" -e "CREATE DATABASE IF NOT EXISTS $db_name;"
-# mysql --user=root -p"$db_root_pwd" -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd';"
-# mysql --user=root -p"$db_root_pwd" -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';"
-# mysql --user=root -p"$db_root_pwd" -e "FLUSH PRIVILEGES;"
-
-# kill $(cat /var/run/mysqld/mysqld.pid)
-
-# sleep 5
-
-# /usr/bin/mysqld_safe
-
-mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
-chmod 755 /run/mysqld
-
-
-mv /tmp/init.sql /etc/mysql/init.sql
+sleep 5
 
 /usr/bin/mysqld_safe
